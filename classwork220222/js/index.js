105bb45e8481ae5070f14299f6c73030a7435b70
Mila2601 <shulandtmila@gmail.com>
2022-02-23 10:00:30 +0200

    make some changes in last classwork (init arr by Constructor and add elements to arr with loop)
    make all homework

--- classwork220222/js/index.js
+++ classwork220222/js/index.js
@@ -1,4 +1,8 @@
-let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let arr = new Array(15);
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = i; 
+}
 
 document.write(arr.reverse().join(' &#9729; '));
 
