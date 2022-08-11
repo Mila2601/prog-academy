0dbfb18062f61f80d30ccc4bdbaa98b8d4f2bdb0
Mila2601 <shulandtmila@gmail.com>
2022-08-11 12:45:45 +0200

    new homework 7-8
    arrow functions, currencies, transfer icons

--- FE Pro 12.07.22/hw/homework.js
+++ FE Pro 12.07.22/hw/homework.js
@@ -2,11 +2,9 @@ let countries = [];
 let storedCountries = [];
 
 function checkSelect() {
-    document.querySelector('.countries-select').onchange = function(e) {
+    document.querySelector('.countries-select').onchange = e => {
         const value = e.currentTarget.value;
-        const filteredCountries = countries.filter(function(country) {
-            return country.region === value;
-        })
+        const filteredCountries = countries.filter(country => country.region === value);
         renderCountries(filteredCountries.length ? filteredCountries : countries);
         storedCountries = filteredCountries;
         document.getElementById('search').value = '';
@@ -15,36 +13,29 @@ function checkSelect() {
 }
 
 function renderSelect(countries) {
-    const uniqueRegions = countries.reduce(function(acc, country) {
+    const uniqueRegions = countries.reduce((acc, country) => {
         if(!acc.includes(country.region)) {
             acc.push(country.region);
         }
         return acc;
     }, []);
     let htmlStr = `<option value="">Select Region</option>`;
-    htmlStr += uniqueRegions.map(function(region) {
-        return `<option value="${region}">${region}</option>`;
-    }).join('');
-
-    let selectElement = document.createElement('select');
-    selectElement.className = "countries-select form-control my-3";
+    htmlStr += uniqueRegions.map(region => (`<option value="${region}">${region}</option>`)).join('');
+    let selectElement = document.querySelector('select');
     selectElement.innerHTML = htmlStr;
-    document.querySelector('#search').before(selectElement);
     checkSelect();
 }
 
 function getCountries() {
-    fetch('https://restcountries.com/v2/all').then(function (data){
-    return data.json();
-}).then(function(data) {
-    countries = data.map(function(country) {
-        return {
+    fetch('https://restcountries.com/v2/all').then(data => data.json()).then((data = []) => {
+    countries = data.map(country => ({
             name: country.name,
             capital: country.capital || '',
             area: country.area || 0,
-            region: country.region
-        }
-    });
+            region: country.region,
+            currency: (() => { return (country.currencies) ? country.currencies[0].code : '---' })()
+        }))
+
     const countriesSelect = document.querySelector('.countries-select');
     if(countriesSelect?.innerHTML) {
         countriesSelect.remove();
@@ -55,25 +46,24 @@ function getCountries() {
 }
 
 function renderCountries(countries) {           
-    const htmlStr = countries.reduce(function(acc, country, index) {
-        return acc + `<tr>
+    const htmlStr = countries.reduce((acc, country, index) => acc + `<tr>
                     <td>${index + 1}</td>
                     <td>${country.name}</td>
                     <td>${country.capital || '---'}</td>
                     <td>${country.region}</td>
                     <td>${country.area}</td>
-                </tr>`;
-    }, '');
+                    <td>${country.currency}</td>
+                </tr>`, '');
     document.querySelector('.table tbody').innerHTML = htmlStr;
     setListeners();
 }
 
 function setListeners() {
-    document.getElementById('search').onkeyup = function(e) {
+    document.getElementById('search').onkeyup = e => {
         clearIcon();
         let searchValue = e.currentTarget.value.toLowerCase().trim();
         const filteredCountries = (storedCountries.length ? storedCountries : countries)
-            .filter(function(country) {
+            .filter(country => {
             const name = country.name.toLowerCase();
             const capital = country.capital.toLowerCase();
             const region = country.region.toLowerCase();
@@ -84,7 +74,7 @@ function setListeners() {
         renderCountries(filteredCountries);
     }
     let tbody = document.querySelector('.table tbody');
-    tbody.onclick = function(e) { 
+    tbody.onclick = e => { 
         for(let item of document.querySelectorAll('table tbody tr')) {
             item.classList.remove('bg-warning');
         }        
@@ -94,26 +84,23 @@ function setListeners() {
 }
 
 function clearIcon() {
-    for (let i of document.querySelectorAll('th i')) {
-        i.className = '';
+    for (let i of document.querySelectorAll('.fa')) {
+        i.classList.add('d-none');
     }
 }
 
 function setIcon() {
     for (let item of document.querySelectorAll('[data-attr]')) {
-        item.onclick = function(e) {
+        item.onclick = e => {
             clearIcon();
             let key = e.currentTarget.getAttribute('data-attr');
             let isSorted = e.currentTarget.getAttribute('data-sort');
-            e.currentTarget.querySelector('th i').classList.add('fa');           
             
             if (isSorted) {
-                e.currentTarget.querySelector('th i').classList.add('fa-sort-amount-asc');
-                e.currentTarget.querySelector('th i').classList.remove('fa-sort-amount-desc');
+                e.currentTarget.querySelector('.fa-sort-amount-asc').classList.remove('d-none');               
                 e.currentTarget.removeAttribute('data-sort');
             } else {
-                e.currentTarget.querySelector('th i').classList.add('fa-sort-amount-desc');
-                e.currentTarget.querySelector('th i').classList.remove('fa-sort-amount-asc');
+                e.currentTarget.querySelector('.fa-sort-amount-desc').classList.remove('d-none');
                 e.currentTarget.setAttribute('data-sort', '+');
             };
             sortCountries(key, isSorted);
@@ -122,7 +109,7 @@ function setIcon() {
 }
 
 function sortCountries(key, isSorted) {
-    let rendered = (storedCountries.length ? storedCountries : countries).sort(function (a, b) {
+    let rendered = (storedCountries.length ? storedCountries : countries).sort((a, b) => {
         if (isSorted) {
             return (a[key] > b[key]) ? 1 : -1
         } else {
