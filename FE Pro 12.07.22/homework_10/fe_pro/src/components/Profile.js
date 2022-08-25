3f0712de668d03b5cacb63b9c64f58932ecbf965
Mila2601 <shulandtmila@gmail.com>
2022-08-25 22:45:57 +0200

    useContext & ref in Forms

--- FE Pro 12.07.22/homework_10/fe_pro/src/components/Profile.js
+++ FE Pro 12.07.22/homework_10/fe_pro/src/components/Profile.js
@@ -1,35 +1,46 @@
 import { Button, Form } from "react-bootstrap";
-import { React, useRef} from 'react'
+import { React, useContext, useRef} from 'react';
+import ProfileContext from "../context/ProfileContext";
 
 function Profile () {
     let loginRef = useRef(null);
     let emailRef = useRef(null);
+    const {profile, setProfile} = useContext(ProfileContext);
 
     function saveProfile() {
-        setProfile({login: localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).login.length ? 
-                           JSON.parse(localStorage.getItem('profile')).login : 
-                           loginRef.current.value, 
-                    email: localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).email.length ? 
-                           JSON.parse(localStorage.getItem('profile')).email : 
-                           emailRef.current.value});
-        localStorage.setItem('profile', JSON.stringify(profile));
-        emailRef.current.value = "";
-        loginRef.current.value = "";
+        let reg = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+        console.log(reg.test(emailRef));
+        // К сожалению тест всегда приводит к false. Я не придумала как правильно проверить имейл и проверяю просто по длине.
+        if (loginRef.current.value.length > 2 && emailRef.current.value.length > 7) { 
+            setProfile({login: localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).login.length ? 
+                            JSON.parse(localStorage.getItem('profile')).login : 
+                            loginRef.current.value, 
+                        email: localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).email.length ? 
+                            JSON.parse(localStorage.getItem('profile')).email : 
+                            emailRef.current.value});
+            localStorage.setItem('profile', JSON.stringify(profile));
+            //emailRef.current.value = "";
+            //loginRef.current.value = "";
+            document.querySelector('.confirmation').classList.remove('d-none');
+            document.querySelector('.formProfile').classList.add('d-none')} else {alert(`Введіть правильні логін та email-адресу`)}
     }
 
     return <div className="container">
-        <Form>
-            <h3>Будь ласка, введіть свої данні:</h3>
-            <Form.Control className="mt-4" 
-                        type={'text'}
-                        ref={loginRef} 
-                        placeholder="Введіть свій логін"></Form.Control>
-            <Form.Control className="mt-4" 
-                        type={'email'} 
-                        ref={emailRef} 
-                        placeholder="Введіть вашу email-адресу"></Form.Control>
-            <Button onClick={saveProfile} className="mt-4" variant={'success'}>Підтвердити</Button>
-        </Form>
+        {localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).login.length ? 
+            <p>{`Вітаємо, ${JSON.parse(localStorage.getItem('profile')).login}!`}</p> :
+            <Form className="formProfile">
+                <h3>Будь ласка, введіть свої данні:</h3>
+                <Form.Control className="mt-4" 
+                            type={'text'}
+                            ref={loginRef} 
+                            placeholder="Введіть свій логін"></Form.Control>
+                <Form.Control className="mt-4" 
+                            type={'email'} 
+                            ref={emailRef} 
+                            placeholder="Введіть вашу email-адресу"></Form.Control>
+                <Button onClick={saveProfile} className="mt-4" variant={'success'}>Підтвердити</Button>
+            </Form>}
+        <div className="d-none confirmation mt-4">{`Дякуємо, ${profile.login}, Вашу адресу ${profile.email} було записано!`}</div>
     </div>
 }
 
