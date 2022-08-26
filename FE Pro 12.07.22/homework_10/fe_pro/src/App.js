e6a1f42fc24a510ea4196bd3ff917e39653cd3b8
Mila2601 <shulandtmila@gmail.com>
2022-08-26 18:10:03 +0200

    Not working version. An empty profile is written.

--- FE Pro 12.07.22/homework_10/fe_pro/src/App.js
+++ FE Pro 12.07.22/homework_10/fe_pro/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Products from './components/Products';
 import ProfileContext from './context/ProfileContext';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [profile, setProfile] = useState({login: '', email: ''});
 
+  useEffect(() => {
+    const savedProfile = JSON.parse(localStorage.getItem('profile'));
+    if(savedProfile?.login?.length) {setProfile(savedProfile)}
+    }, [])
   return (
     <div className='bg-dark p-4' id='app'>
       <ProfileContext.Provider value={{profile, setProfile}}>
--- FE Pro 12.07.22/homework_10/fe_pro/src/components/Profile.js
+++ FE Pro 12.07.22/homework_10/fe_pro/src/components/Profile.js
@@ -9,34 +9,29 @@ function Profile () {
 
     function saveProfile() {
         let reg = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
-        console.log(reg.test(emailRef));
-        // К сожалению тест всегда приводит к false. Я не придумала как правильно проверить имейл и проверяю просто по длине.
         if (loginRef.current.value.length > 2 && emailRef.current.value.length > 7) { 
-            setProfile({login: localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).login.length ? 
-                            JSON.parse(localStorage.getItem('profile')).login : 
-                            loginRef.current.value, 
-                        email: localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).email.length ? 
-                            JSON.parse(localStorage.getItem('profile')).email : 
-                            emailRef.current.value});
+            setProfile({login: profile.login.length ? profile.login : loginRef.current.value, 
+                        email: profile.email.length ? profile.email : emailRef.current.value});
+            console.log(profile);
             localStorage.setItem('profile', JSON.stringify(profile));
-            //emailRef.current.value = "";
-            //loginRef.current.value = "";
             document.querySelector('.confirmation').classList.remove('d-none');
             document.querySelector('.formProfile').classList.add('d-none')} else {alert(`Введіть правильні логін та email-адресу`)}
     }
 
     return <div className="container">
-        {localStorage.getItem('profile') && JSON.parse(localStorage.getItem('profile')).login.length ? 
-            <p>{`Вітаємо, ${JSON.parse(localStorage.getItem('profile')).login}!`}</p> :
+        {profile && profile.login.length ? 
+            <p>{`Вітаємо, ${profile.login}!`}</p> :
             <Form className="formProfile">
                 <h3>Будь ласка, введіть свої данні:</h3>
                 <Form.Control className="mt-4" 
                             type={'text'}
                             ref={loginRef} 
+                            defaultValue={profile.login}
                             placeholder="Введіть свій логін"></Form.Control>
                 <Form.Control className="mt-4" 
                             type={'email'} 
                             ref={emailRef} 
+                            defaultValue={profile.email}
                             placeholder="Введіть вашу email-адресу"></Form.Control>
                 <Button onClick={saveProfile} className="mt-4" variant={'success'}>Підтвердити</Button>
             </Form>}
